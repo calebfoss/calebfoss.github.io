b497758ecf1b2365dc0d0d7c9d3d9012040d2d8f
calebfoss <calebfoss@gmail.com>
2017-02-12 21:11:59 -0600

    Back to the drawing board

--- CPDP_P5.js
+++ CPDP_P5.js
@@ -1,11 +1,11 @@
 var capture;
 
-function setup(){
-	createCanvas(windowWidth, windowHeight);
-capture = createCapture({
-	video: {deviceId: 'camera 0, facing back'}});
+function setup() {
+  createCanvas(480, 120);
+  capture = createCapture(VIDEO);
 }
 
-function draw(){
-	image(capture,0,0)
+function draw() {
+  image(capture, 0, 0, width, width*capture.height/capture.width);
+  filter(INVERT);
 }
