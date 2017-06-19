a65ac5872b0364513955e22a853171cf3b9329af
calebfoss <calebfoss@gmail.com>
2017-06-19 15:05:30 -0500

    ReUploaded ACPD

--- ACPD/CPDP.js
+++ ACPD/CPDP.js
@@ -1,36 +1,58 @@
 var Data;
-var lat = 41.880882;
-var lng = -87.62087;
+var LatLng;
+var mileCount;
+var earliestYear;
+var latestYear;
+var isChrome = !!window.chrome && !!window.chrome.webstore;
 
-function getLocation() {
+function getGPSLocation() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(showPosition);
 	} else {
 		document.getElementById("text").innerHTML = "Geolocation is not supported by this browser.";
 	}
 }
-
-function showPosition(position) {
-	var changedPosition = false;
-	if(lat != position.coords.latitude){
-		// lat = position.coords.latitude;
-		changedPosition = true;
-	}
-	if(lng != position.coords.longitude){
-		// lng = position.coords.longitude;
-		changedPosition = true;
-	}
-	if(changedPosition) findClosest(lat, lng); 
+function getTypeLocation() {
+	var address = document.getElementById('TypeAddress').value;
+	console.log(address);
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == 'OK') {
+			LatLng = results[0].geometry.location;
+			document.getElementById('TypeAddress').value = '';
+			document.getElementById('AddressFound').innerHTML = 'Showing result for '+results[0].formatted_address+':';
+			findClosest(LatLng);
+		} else {
+			alert('Geocode was not successful for the following reason: ' + status);
+		}
+	});
 }
 
-function findClosest(lat, lng){
+function showPosition(position) {
+	document.getElementById('AddressFound').innerHTML = '';
+	LatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	findClosest(LatLng);
+}
+
+function findClosest(LatLng){
+	mileCount = 0;
+	earliestYear = Infinity;
+	latestYear = 0;
 	var closestIndex = 0;
 	var closestDistance = Infinity;
 	for(var i = 0; i < Data.length; i++){
-		var newDistance = distance(lat,lng,Data[i].lat,Data[i].lng);
+		var newDistance = distance(LatLng,Data[i].lat,Data[i].lng);
 		if(newDistance<closestDistance){
 			closestIndex = i;
 			closestDistance = newDistance;
+		}
+		if(newDistance<1609.34){
+			mileCount++;
+			var year = parseInt(moment(Data[i].incident_date,'M/D/YYYY H:mm').format('YYYY'));
+			if(year < earliestYear)
+				earliestYear = year;
+			if(year > latestYear)
+				latestYear = year;
 		}
 	}
 	generateText(Data[closestIndex], closestDistance);
@@ -40,22 +62,31 @@ window.onload = function(){
 	d3.csv("data.csv", function(data) {
 		Data = data;
 	});
-	getLocation();
+	if(!isChrome){
+		document.getElementById('GPS').style.visibility = "visible"; 
+	}
+	document.getElementById("text").innerHTML = '';
 }
 
-function distance(lat1,lng1,lat2,lng2){
-	var from = new google.maps.LatLng(lat1, lng1);
+function distance(LatLng,lat2,lng2){
+	var from = LatLng;
 	var to   = new google.maps.LatLng(lat2, lng2);
 	var dist = google.maps.geometry.spherical.computeDistanceBetween(from, to);
 	return dist;
 }
 
-function cardinal(lat1,lng1,lat2,lng2){
-	var from = new google.maps.LatLng(lat1, lng1);
+function cardinal(LatLng,lat2,lng2){
+	var from = LatLng;
 	var to   = new google.maps.LatLng(lat2, lng2);
 	var bearings = ['north','northeast','east','southeast','south','southwest','west','northwest'];
 	var heading = google.maps.geometry.spherical.computeHeading(from,to);
-	var card = bearings[Math.round(heading/360)];
+	var index = Math.round(heading/45);
+	if(index < 0)
+		index += 7;
+	if(index > 7)
+		index = 0;
+	var card = bearings[index];
+	console.log(heading/360);
 	return card;
 }
 function checkPresent(value){
@@ -76,7 +107,7 @@ function generateText(data,dist){
 	}
 	var text = Math.round(dist*3.28084)
 	+" feet "
-	+cardinal(lat,lng,data.lat,data.lng)
+	+cardinal(LatLng,data.lat,data.lng)
 	+" of you at "
 	+data.add1+" "+Humanize.capitalizeAll(data.add2)
 	+", an incident took place on "
@@ -97,7 +128,7 @@ function generateText(data,dist){
 			if(data.gender_comp == 'm')
 				text+='male';
 			if(data.gender_comp == 'f')
-				text+='female;'
+				text+='female'
 			if(checkPresent(data.age)){
 				if(checkPresent(data.race_edit_comp)){
 					text+=', ';
@@ -105,6 +136,9 @@ function generateText(data,dist){
 				else{
 					text+=' and '
 				}
+			}
+			else if(checkPresent(data.race_edit_comp)){
+				text+=' and '
 			}
 		}
 		else
@@ -127,47 +161,66 @@ function generateText(data,dist){
 		}
 		text+='. '
 	}
-if(checkPresent(data.officer_id)){
-	text+='The accused officer was '+Humanize.capitalizeAll(data.officer_first+' '+data.officer_last)
-	+', who was ';
-	if(data.gender_off == 'm')
-		text+='male, ';
-	if(data.gender_off =='f')
-		text+='female, ';
-	text+=data.race_edit_off+', and ';
-	text+=(moment(dateSplit[0],'MM/DD/YYYY').format('YYYY')-data.birth_year)+' years old. ';
+	if(checkPresent(data.officer_id)){
+		text+='The accused officer was '+Humanize.capitalizeAll(data.officer_first+' '+data.officer_last)
+		+', who was ';
+		if(data.gender_off == 'm')
+			text+='male, ';
+		if(data.gender_off =='f')
+			text+='female, ';
+		text+=data.race_edit_off+', and ';
+		text+=(moment(dateSplit[0],'MM/DD/YYYY').format('YYYY')-data.birth_year)+' years old. ';
 
-}
-else{
-	missing.push('information about the accused officer');
-}
-if(checkPresent(data.final_outcome_highest)){
-	if(data.final_outcome_highest!='open-investigation')
-	text+='The allegation was found '+data.final_outcome_highest+'. ';
-else
-	text+='As of December 7th, 2015, the allegation was still under investigation. ';
-}
-else
-missing.push('the result of the allegation');
-if(checkPresent(data.officer_id)){
-	text+=Humanize.capitalize(data.officer_last)+' has had '+data.allegations_count+' allegations filed on them. They have been disciplined '
-	+data.discipline_count+' times. ';
-	if(data.Active_June_1_2015=='yes')
-		text+=Humanize.capitalize(data.officer_last)+' was still active in the CPD as of June 1st, 2015. ';
-	if(data.Active_June_1_2015=='no')
-		text+=Humanize.capitalize(data.officer_last)+' is no longer active in the CPD as of June 1st, 2015. ';
-}
-if(missing.length > 0){
-	text+='The complaint was missing ';
-	for(var i = 0; i < missing.length; i++){
-		text+=missing[i];
-		if(i<missing.length-2)
-			text+=', ';
-		else if(i<missing.length-1)
-			text+=', and ';
 	}
-	text+='.'
+	else{
+		missing.push('information about the accused officer');
+	}
+	if(checkPresent(data.final_outcome_highest)){
+		if(data.final_outcome_highest!='open-investigation')
+			text+='The allegation was found '+data.final_outcome_highest+'. ';
+		else
+			text+='As of December 7th, 2015, the allegation was still under investigation. ';
+	}
+	else
+		missing.push('the result of the allegation');
+	if(checkPresent(data.officer_id)){
+		text+=Humanize.capitalize(data.officer_last)+' has had '+data.allegations_count+' allegations filed on them. ';
+		var discipline = data.discipline_count;
+		if(discipline == 0)
+			text+='They have never been disciplined. ';
+		else if(discipline == 1)
+			text+='They have been disciplined once. ';
+		else
+			text+='They have been disciplined '
+		+data.discipline_count+' times. ';
+		if(data.Active_June_1_2015=='yes')
+			text+=Humanize.capitalize(data.officer_last)+' was still active in the CPD as of June 1st, 2015. ';
+		if(data.Active_June_1_2015=='no')
+			text+=Humanize.capitalize(data.officer_last)+' is no longer active in the CPD as of June 1st, 2015. ';
+	}
+	if(missing.length > 0){
+		text+='<br><br>The complaint was missing ';
+		for(var i = 0; i < missing.length; i++){
+			text+=missing[i];
+			if(i<missing.length-2)
+				text+=', ';
+			else if(i<missing.length-1)
+				text+=', and ';
+		}
+		text+='.'
+	}
+	text+='<br><br>'+(mileCount-1)+' other allegations have been filed less than one mile from your location between '
+	+earliestYear+' and '+latestYear+'. ';
+	console.log(data);
+	document.getElementById("text").style.opacity = 0;
+	fadeIn('text');
+	document.getElementById("text").innerHTML = text;
+	document.getElementById('GPS').innerHTML = 'Refresh GPS location';
 }
-console.log(data);
-document.getElementById("text").innerHTML = text;
+
+function fadeIn(id){
+	if(document.getElementById(id).style.opacity < 1){
+		document.getElementById(id).style.opacity = parseFloat(document.getElementById(id).style.opacity) + 0.0025;
+		setTimeout(function(){fadeIn(id)},1);
+	}
 }
