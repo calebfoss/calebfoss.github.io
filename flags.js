c2dbaf3707c7955028521942c6f767b39a8ddd92
caleb-cdm <cfoss1@depaul.edu>
2021-06-04 11:34:19 -0500

    Add queer flag encoding app

---
+++ flags.js
@@ -0,0 +1,13 @@
+const flags = {
+  lgbtq: ["#FF0018", "#FFA52C", "#FFFF41", "#008018", "#0000F9", "#86007D"],
+  lesbian: ["#D62900", "#FF9B55", "#FFFFFF", "#D461A6", "#A50062"],
+  bisexual: ["#D00070", "#8C4799", "#0032A0"],
+  transgender: ["#3bdcff", "#f9c5c5",	"#ffffff"],
+  intersex: ["#d8abcf", "#ffffff", "#a1cdee", "#f0b7d6"],
+  asexual: ["#000000", "#A4A4A4", "#FFFFFF", "#810081"], 
+  "non-binary": ["#fff433", "#ffffff", "#9b59d0", "#000000"],
+  pangender: ["#FFEA7E", "#FFC3B0", "#FFD6F3", "#FFFFFF"],
+  demiromantic: ["#000000", "#ffffff", "#d2d2d2", "#329932"],
+  agender: ["#d3d3d3", "#e7e7e7", "#ffffff", "#e1fec8", "#ccfea2"],
+  genderfae: ["#95c4a4", "#c2deae", "#faf9ce", "#fca3c4", "#db8ae3", "#ab7edd"], polysexual: ["#F714BA", "#01D66A", "#1594F6"], polyamorous: ["#2838cf", "#e41e1e", "#000000"]
+};
