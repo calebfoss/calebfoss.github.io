96081a45b8ae09f39cf1a4b4242860bf7e9868f8
caleb-cdm <cfoss1@depaul.edu>
2021-10-15 15:24:58 -0500

    Add queer and genderqueer

--- flags.js
+++ flags.js
@@ -7,6 +7,7 @@ const flags = {
   demisexual: ["#000000", "#FFFFFF", "#A4A4A4", "#810081"],
   genderfae: ["#95c4a4", "#c2deae", "#faf9ce", "#fca3c4", "#db8ae3", "#ab7edd"],
   genderfluid: ["#FF76A4", "#FFFFFF", "#C011D7", "#000000", "#2F3CBE"],
+  genderqueer: ["#B77FDD", "#FFFFFF", "#48821E"],
   "LGBTQ rainbow": ["#FF0018", "#FFA52C", "#FFFF41", "#008018", "#0000F9", "#86007D"],
   intersex: ["#d8abcf", "#ffffff", "#a1cdee", "#f0b7d6"],
   lesbian: ["#D62900", "#FF9B55", "#FFFFFF", "#D461A6", "#A50062"],
@@ -16,5 +17,6 @@ const flags = {
   pansexual: ["#FF1B8D", "#FFDA00", "#1BB3FF"],
   polyamorous: ["#2838cf", "#e41e1e", "#000000"],
   polysexual: ["#F714BA", "#01D66A", "#1594F6"],
+  queer: ["#000000", "#9cd9eb", "#00a2ea", "#b6e51d", "#ffffff", "#ffc80f", "#fc6667", "#feaec9", "#000000"],
   transgender: ["#3bdcff", "#f9c5c5", "#ffffff"],
 };
